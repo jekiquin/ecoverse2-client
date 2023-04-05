aa23a83b6cfd2664605d127adc5fcbcf704726de
Jerick Iquin <jci.dev.0@gmail.com>
2023-04-05 13:35:25 -0700

    added basic onboarding and notfound pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 
 import Home from './Home';
+import NotFound from './NotFound';
 import Projects from './Projects';
 
 export const routes: RouteObject[] = [
   {
     path: '/',
     element: <Home />,
+    errorElement: <NotFound />,
   },
   {
     path: '/projects',
