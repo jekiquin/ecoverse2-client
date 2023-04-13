c6ee85b012f6e8bc0684953b2e0539f86aa76580
jekiquin <jci.dev.0@gmail.com>
2023-04-13 10:45:00 -0700

    replaced useToolTip hook with just css

--- src/components/SideNav/NavItem.tsx
+++ src/components/SideNav/NavItem.tsx
@@ -1,9 +1,8 @@
-import React, { ReactElement, useEffect, useRef } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
 
 import { useNavContext } from '@/context/NavContext';
-import useShowToolTip from '@/hooks/useShowToolTip';
 import { defaultClassNameProps } from '@/utils/constants';
 
 import { INavItem } from './types';
@@ -21,26 +20,14 @@ export default function NavItem({
   isPrivate, // this will be used somewhere
   className,
 }: Props) {
-  const { showTooltip, setElement } = useShowToolTip();
   const { navOpen } = useNavContext();
 
-  const itemRef = useRef<HTMLLIElement | null>(null);
-
-  useEffect(() => {
-    setElement(itemRef.current);
-  }, []);
-
   return (
-    <li ref={itemRef} className={twMerge(styles.root, className)}>
+    <li className={twMerge(styles.root, className)}>
       <NavLink to={href} className={styles.link}>
         <Icon className={styles.icon} />
         <p className={styles.label}>{label}</p>
-        <p
-          className={twMerge(
-            styles.tooltip,
-            showTooltip && !navOpen && styles.showTooltip
-          )}
-        >
+        <p className={twMerge(styles.tooltip, navOpen && styles.tooltipClosed)}>
           {label}
         </p>
       </NavLink>
@@ -49,11 +36,10 @@ export default function NavItem({
 }
 
 const styles = {
-  root: 'overflow-hidden h-6 bg-white',
+  root: 'overflow-hidden h-6 bg-white custom-tooltip',
   link: 'flex items-center gap-4 h-full',
   icon: 'h-full w-auto grow-0 shrink-0',
   label: 'shrink-1 w-full',
-  tooltip:
-    'absolute -z-10 left-full px-4 py-2 rounded-lg opacity-0 translate-x-1 transition-opacity bg-white',
-  showTooltip: 'z-10 opacity-100',
+  tooltip: 'custom-tooltip__ctx',
+  tooltipClosed: 'custom-tooltip__ctx--closed',
 };
